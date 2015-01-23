daa69562fd928eced86b05deefc9987ad202da9f
Samuel Hiatt <samhiatt@gmail.com>
2015-01-23 23:23:03 +0000

    Added onReady, onEnd, onError, and onClose event handlers to config object.

--- index.js
+++ index.js
@@ -28,13 +28,20 @@ SSHTunnel.prototype.close = function (callback) {
 };
 
 SSHTunnel.prototype.connect = function (callback) {
-    callback = (callback)? callback : function(err){if (err) this.log(err);}.bind(this);
     var self = this,
         disabled = self._config.disabled,
         remotePort = self._config.remotePort,
         localPort = self._config.localPort,
-        remoteHost = self._config.remoteHost || '127.0.0.1';
-
+        remoteHost = self._config.remoteHost || '127.0.0.1',
+        onReady = self._config.onReady,
+        onError = self._config.onError,
+        onClose = self._config.onClose,
+        onEnd = self._config.onEnd;
+        
+    callback = callback || function(err){
+        if (err) {self.log(err);}
+        else { if (typeof onReady == 'function') onReady(); }
+    };
 
     if(disabled){
         return callback(null);
@@ -42,6 +49,7 @@ SSHTunnel.prototype.connect = function (callback) {
     var c = self.connection = new Connection();
 
     c.on('ready', function () {
+        self.log("Connected "+self._config.sshConfig.host+":"+remotePort+" to local port "+localPort);
 
         self.server = net.createServer(function (connection) {
             var buffers = [];
@@ -90,18 +98,22 @@ SSHTunnel.prototype.connect = function (callback) {
         // handle error when self.server.listen fails (e.g. port is already used)
         self.server.on('error', function(err){
             callback(err);
+            if (typeof onError == 'function') onError(err);
             self.connection.end();
         });
     });
 
     c.on('error', function (err) {
         self.log('ssh2::error:: ' + err);
+        if (typeof onError == 'function') onError(err);
         callback(err);
     });
     c.on('end', function () {
+        if (typeof onEnd == 'function') onEnd();
         self.log('ssh2::end');
     });
     c.on('close', function (err) {
+        if (typeof onClose == 'function') onClose();
         self.log('ssh2::close');
     });
 
