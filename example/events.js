26e7065e1f4a157a55381f142ad9ee054fb14dd8
Christoph Hagenbrock <christoph.hagenbrock@googlemail.com>
2015-03-20 08:40:35 +0100

    Add examples.

--- example/events.js
+++ example/events.js
@@ -1,4 +1,12 @@
+/**
+ *
+ * Every object created by the tunnel is emitted through the "server" instance.
+ * Every instance event has the same order of object but they differ in length.
+ *
+ *
+ */
 var tunnel = require('../');
+
 
 var config = {
     host: '93.180.157.151',
@@ -11,7 +19,11 @@ var server = tunnel(config, function () {
     console.log('connected');
 });
 
+/**
+ *  left to right => small to large (server is the biggest / root,  sshStream the smnallest)
+ */
 server.on('sshStream', function (sshStream, sshConnection, netConnection, server) {
+
     sshStream.on('close', function () {
         console.log('TCP :: CLOSED');
             netConnection.end();
--- example/mysql.js
+++ example/mysql.js
@@ -1,21 +1,30 @@
 var tunnel = require('../');
 
+
+
 var config = {
-    host: '93.180.157.151',
+    host: '172.16.0.8',
     username: 'root',
-    dstPort: 27017,
-    localPort: 3000
+    dstPort: 3306
 };
 
-var server = tunnel(config, function () {
+var server = tunnel(config, function (error, result) {
     console.log('connected');
 });
 
+
+var i = 0;
 server.on('sshStream', function (sshStream, sshConnection, netConnection, server) {
     sshStream.on('close', function () {
         console.log('TCP :: CLOSED');
+        console.log(i);
+        if(i === 1){
             netConnection.end();
             server.close();
+        }
+        i++;
+    }).on('data', function (data) {
+        console.log('TCP :: DATA: ');
     });
 });
 
