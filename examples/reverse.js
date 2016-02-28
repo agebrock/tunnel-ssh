427f34373236a5e68b04ac6f8cbbf7aebaf174e5
Christoph Hagenbrock <christoph.hagenbrock@googlemail.com>
2016-02-28 22:34:36 +0100

    Improve example to show a more practical usecase.

--- examples/reverse.js
+++ examples/reverse.js
@@ -1,11 +1,31 @@
 var tunnel = require('../');
 
+
+// This is a very handy way to test your next webhook !
+
+
+// Please set up your /etc/hosts or change the hostname befor
+// running the example.
+
+
+// The name "local" is currently missleading since you can put
+// remove ports as well...
+var dstPort = 8000;
+var host = 'tunneltest.com';
+
 tunnel.reverse({
-  host: 'agebrock.com',
+  host: host,
   username: 'root',
-  dstHost: '0.0.0.0',
-  dstPort: 3000,
-  localPort: 8000
-}, function() {
-  console.log(arguments);
+  dstHost: '0.0.0.0', // bind to all interfaces (see hint in the readme)
+  dstPort: dstPort,
+  //localHost: '127.0.0.1', // default
+  //localPort: localPort
+}, function(error, clientConnection) {
+  console.log(clientConnection._forwarding);
 });
+
+require('http').createServer(function(res, res){
+  res.end('SSH-TUNNEL: Gate to heaven !');
+}).listen(localPort);
+
+console.log('Tunnel created: http://'+host+':'+localPort);
