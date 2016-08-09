41d0ccf3a69fb2450b5c0d51b9fd41b2a8df3d56
Christoph Hagenbrock <christoph.hagenbrock@googlemail.com>
2016-08-09 16:32:07 +0200

    refactor: clean up examples

--- examples/default.js
+++ examples/default.js
@@ -1,24 +1,18 @@
 var tunnel = require('../');
 var helper = require('./server');
 
-
 var config = {
-  host: '127.0.0.1',
-  username: process.env.USER,
-  dstPort: 8000,
-  localPort: 7000
+    host: '127.0.0.1', username: process.env.USER, dstPort: 8000, localPort: 7000
 };
 
-var fakeServer = helper.createServer(config.dstPort, '127.0.0.1', function() {
-
-  tunnel(config, function() {
-    console.log('Tunnel open');
-    helper.createClient(7000, '127.0.0.1', console.log);
-    helper.createClient(7000, '127.0.0.1', console.log);
-  }).on('error', function(e) {
-    console.log('error', e);
-  });
-
+var fakeServer = helper.createServer(config.dstPort, '127.0.0.1', function () {
+    tunnel(config, function () {
+        console.log('Tunnel open');
+        helper.createClient(7000, '127.0.0.1', console.log);
+        helper.createClient(7000, '127.0.0.1', console.log);
+    }).on('error', function (e) {
+        console.log('error', e);
+    });
 });
 
 fakeServer.unref();
--- examples/keepAlive.js
+++ examples/keepAlive.js
@@ -1,6 +1,4 @@
 var tunnel = require('../');
-var net = require('net');
-var debug = require('debug')('tunnel-ssh:test');
 var helper = require('./server');
 
 // Keep alive example
@@ -10,34 +8,28 @@ var helper = require('./server');
 // this step is required to finish execution nicely
 
 var configA = {
-  host: '127.0.0.1',
-  username: process.env.USER,
-  dstPort: 8000,
-  localPort: 7000,
-  // Use keepAlive:true to keep the tunnel open.
-  keepAlive: true
+    host: '127.0.0.1', username: process.env.USER, dstPort: 8000, localPort: 7000, // Use keepAlive:true to keep the tunnel open.
+    keepAlive: true
 };
 
-var fakeServer = helper.createServer(configA.dstPort, '127.0.0.1', function() {
-
-  var tunnelKeepAlive = tunnel(configA, function() {
-    console.log('Tunnel open');
-    helper.createClient(7000, '127.0.0.1', console.log);
-    helper.createClient(7000, '127.0.0.1', console.log);
-    helper.createClient(7000, '127.0.0.1', console.log).on('close', function() {
-      helper.createClient(7000, '127.0.0.1', console.log).on('close', function() {
-        helper.createClient(7000, '127.0.0.1', console.log).on('close', function() {
-          setTimeout(function() {
-            // Call tunnel.close() to shutdown the server.
-            console.log('TRYING TO CLOSE');
-            tunnelKeepAlive.close();
-          }, 2000);
+var fakeServer = helper.createServer(configA.dstPort, '127.0.0.1', function () {
+    var tunnelKeepAlive = tunnel(configA, function () {
+        console.log('Tunnel open');
+        helper.createClient(7000, '127.0.0.1', console.log);
+        helper.createClient(7000, '127.0.0.1', console.log);
+        helper.createClient(7000, '127.0.0.1', console.log).on('close', function () {
+            helper.createClient(7000, '127.0.0.1', console.log).on('close', function () {
+                helper.createClient(7000, '127.0.0.1', console.log).on('close', function () {
+                    setTimeout(function () {
+                        // Call tunnel.close() to shutdown the server.
+                        console.log('TRYING TO CLOSE');
+                        tunnelKeepAlive.close();
+                    }, 2000);
+                });
+            });
         });
-      });
+    }).on('error', function (e) {
+        console.log('error', e);
     });
-  }).on('error', function(e) {
-    console.log('error', e);
-  });
-
 });
 fakeServer.unref();
