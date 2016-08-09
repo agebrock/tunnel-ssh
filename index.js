6d4eb94d4fc0e2443e72e330239b9a96d17f774c
Christoph Hagenbrock <christoph.hagenbrock@googlemail.com>
2016-08-09 16:34:41 +0200

    style: Change codestyle to xo.

--- index.js
+++ index.js
@@ -3,105 +3,99 @@ var debug = require('debug')('tunnel-ssh');
 var Connection = require('ssh2');
 var createConfig = require('./lib/config');
 var events = require('events');
-var noop = function() {
+var noop = function () {
 };
 
 function bindSSHConnection(config, netConnection) {
-  var sshConnection = new Connection();
+    var sshConnection = new Connection();
 
-  sshConnection.on('ready', function() {
-    debug('sshConnection:ready');
-    netConnection.emit('sshConnection', sshConnection, netConnection);
-    sshConnection.forwardOut(
-      config.srcHost,
-      config.srcPort,
-      config.dstHost,
-      config.dstPort, function(err, sshStream) {
-        if (err) {
-          // Bubble up the error => netConnection => server
-          netConnection.emit('error', err);
-          debug('Destination port:', err);
-          return;
-        }
+    sshConnection.on('ready', function () {
+        debug('sshConnection:ready');
+        netConnection.emit('sshConnection', sshConnection, netConnection);
+        sshConnection.forwardOut(config.srcHost, config.srcPort, config.dstHost, config.dstPort, function (err, sshStream) {
+            if (err) {
+                // Bubble up the error => netConnection => server
+                netConnection.emit('error', err);
+                debug('Destination port:', err);
+                return;
+            }
 
-        debug('sshStream:create');
-        netConnection.emit('sshStream', sshStream);
-        netConnection.pipe(sshStream).pipe(netConnection);
-      });
-  });
-  return sshConnection;
+            debug('sshStream:create');
+            netConnection.emit('sshStream', sshStream);
+            netConnection.pipe(sshStream).pipe(netConnection);
+        });
+    });
+    return sshConnection;
 }
 
 function createServer(config) {
-  var server,
-    sshConnection,
-    connections = [],
-    connectionCount = 0;
+    var server;
+    var sshConnection;
+    var connections = [];
+    var connectionCount = 0;
 
-  server = net.createServer(function(netConnection) {
-    connectionCount++;
-    netConnection.on('error', server.emit.bind(server, 'error'));
-    netConnection.on('close', function() {
-
-      connectionCount--;
-      if (connectionCount === 0) {
-        if (!config.keepAlive) {
-          setTimeout(function() {
+    server = net.createServer(function (netConnection) {
+        connectionCount++;
+        netConnection.on('error', server.emit.bind(server, 'error'));
+        netConnection.on('close', function () {
+            connectionCount--;
             if (connectionCount === 0) {
-              server.close();
+                if (!config.keepAlive) {
+                    setTimeout(function () {
+                        if (connectionCount === 0) {
+                            server.close();
+                        }
+                    }, 2);
+                }
             }
-          },2);
-          };
-        }
+        });
+
+        server.emit('netConnection', netConnection, server);
+        sshConnection = bindSSHConnection(config, netConnection);
+        sshConnection.on('error', server.emit.bind(server, 'error'));
+
+        netConnection.on('sshStream', function (sshStream) {
+            sshStream.on('error', function () {
+                server.close();
+            });
+        });
+
+        connections.push(sshConnection, netConnection);
+        debug('sshConfig', config);
+        sshConnection.connect(config);
     });
 
-    server.emit('netConnection', netConnection, server);
-    sshConnection = bindSSHConnection(config, netConnection);
-    sshConnection.on('error', server.emit.bind(server, 'error'));
-
-    netConnection.on('sshStream', function(sshStream) {
-      sshStream.on('error', function() {
-        server.close();
-      });
+    server.on('close', function () {
+        connections.forEach(function (connection) {
+            connection.end();
+        });
     });
 
-    connections.push(sshConnection, netConnection);
-    debug('sshConfig', config);
-    sshConnection.connect(config);
-  });
-
-  server.on('close', function() {
-    connections.forEach(function(connection) {
-      connection.end();
-    });
-  });
-
-  return server;
+    return server;
 }
 
 function tunnel(configArgs, callback) {
-  var server, config;
+    var server;
+    var config;
 
-  if (!callback) {
-    callback = noop;
-  }
-  try {
-    config = createConfig(configArgs);
-    server = createServer(config);
+    if (!callback) {
+        callback = noop;
+    }
+    try {
+        config = createConfig(configArgs);
+        server = createServer(config);
 
-
-    server.listen(config.localPort, config.localHost, function(error) {
-      callback(error, server);
-    });
-
-  } catch (e) {
-    server = new events.EventEmitter();
-    setImmediate(function() {
-      callback(e);
-      server.emit('error', e);
-    });
-  }
-  return server;
+        server.listen(config.localPort, config.localHost, function (error) {
+            callback(error, server);
+        });
+    } catch (e) {
+        server = new events.EventEmitter();
+        setImmediate(function () {
+            callback(e);
+            server.emit('error', e);
+        });
+    }
+    return server;
 }
 
 module.exports = tunnel;
--- test/main-spec.js
+++ test/main-spec.js
@@ -1,75 +1,37 @@
 var tunnel = require('../');
-var net = require('net');
-var debug = require('debug')('tunnel-ssh:test');
 var helper = require('./server');
-var chai = require('chai'),
-  expect = chai.expect;
+var chai = require('chai');
+var expect = chai.expect;
 
+describe('tunnel-ssh', function () {
+    it('should emit an error', function (done) {
+        var config = {
+            host: '127.0.0.1', username: process.env.USER, dstPort: 8000, localPort: 7000
+        };
 
-describe('tunnel-ssh', function() {
-
-  it('should emit an error', function(done) {
-
-    var config = {
-      host: '127.0.0.1',
-      username: process.env.USER,
-      dstPort: 8000,
-      localPort: 7000
-    };
-
-    tunnel(config, function() {
-      helper.createClient(7000, '127.0.0.1', done);
-    }).on('error', function(e) {
-      expect(e).to.be.instanceOf(Error);
-      done(null);
+        tunnel(config, function () {
+            helper.createClient(7000, '127.0.0.1', done);
+        }).on('error', function (e) {
+            expect(e).to.be.instanceOf(Error);
+            done(null);
+        });
     });
-  });
 
-  it('brokenConfig, should callback an error', function(done) {
-    var brokenConfig = {};
+    it('brokenConfig, should callback an error', function (done) {
+        var brokenConfig = {};
 
-    tunnel(brokenConfig, function(e) {
-      expect(e).to.be.instanceOf(Error);
-      done();
+        tunnel(brokenConfig, function (e) {
+            expect(e).to.be.instanceOf(Error);
+            done();
+        });
     });
-  });
 
-  it('brokenConfig, should emit an error', function(done) {
-    var brokenConfig = {};
+    it('brokenConfig, should emit an error', function (done) {
+        var brokenConfig = {};
 
-    tunnel(brokenConfig).on('error', function(e) {
-      expect(e).to.be.instanceOf(Error);
-      done(null);
+        tunnel(brokenConfig).on('error', function (e) {
+            expect(e).to.be.instanceOf(Error);
+            done(null);
+        });
     });
-  });
 });
-
-/*
- // Keep alive
- var configA = {
- host: '127.0.0.1',
- username: process.env.USER,
- dstPort: 8000,
- localPort: 7000,
- // Use keepAlive:true to keep the tunnel open.
- keepAlive: true
- };
- var tunnelKeepAlive = tunnel(configA, function() {
- console.log('Tunnel open');
- helper.createClient(7000, '127.0.0.1', console.log).on('close', function() {
- helper.createClient(7000, '127.0.0.1', console.log).on('close', function() {
- helper.createClient(7000, '127.0.0.1', console.log).on('close', function() {
- setTimeout(function() {
- // Call tunnel.close() to shutdown the server.
- console.log('TRYING TO CLOSE');
- tunnelKeepAlive.close();
- }, 2000);
- });
- });
- });
- }).on('error', function(e) {
- console.log('error', e);
- });
- });
-
- */
