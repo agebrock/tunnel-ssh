28fd601f90b8c82d9a4185dc2ba228ad01ccea11
Christoph Hagenbrock <christoph.hagenbrock@googlemail.com>
2023-01-23 06:44:06 +0100

    improved error handling [#98]

--- index.js
+++ index.js
@@ -2,27 +2,34 @@ const net = require('net');
 const { Client } = require('ssh2');
 
 
-function autoClose(server, connection){
-    connection.on('close',()=>{
-        server.getConnections((error, count)=>{
-            if(count === 0){
+function autoClose(server, connection) {
+    connection.on('close', () => {
+        server.getConnections((error, count) => {
+            if (count === 0) {
                 server.close();
             }
         });
     });
 }
 
-async function createServer(options){
-    return new Promise(function(resolve, reject){
+async function createServer(options) {
+    return new Promise((resolve, reject) => {
         let server = net.createServer();
+        let errorHandler = function (error) {
+            reject(error);
+        };
+        server.on('error', errorHandler);
+        process.on('uncaughtException', errorHandler);
         server.listen(options);
-        server.on('listening', ()=> resolve(server)); 
-        server.on('error', reject);
+        server.on('listening', () => {
+            process.removeListener('uncaughtException', errorHandler);
+            resolve(server);
+        });
     });
 }
 
-async function createClient(config){
-    return new Promise(function(resolve, reject){
+async function createClient(config) {
+    return new Promise(function (resolve, reject) {
         let conn = new Client();
         conn.on('ready', () => resolve(conn));
         conn.on('error', reject);
@@ -30,31 +37,39 @@ async function createClient(config){
     });
 }
 
-async function createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions){
+async function createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions) {
 
-    return new Promise(function(resolve, reject){
-        createServer(serverOptions).then(function(server, reject){
-            createClient(sshOptions).then(function(conn){
-               
-                server.on('connection',(connection)=>{
-                    
-                    if(tunnelOptions.autoClose){
-                        autoClose(server, connection);
-                    }
+    return new Promise(async function (resolve, reject) {
+        let server, conn;
+        try {
+            server = await createServer(serverOptions);
+        } catch (e) {
+            return reject(e);
+        }
 
-                    conn.forwardOut(
-                        forwardOptions.srcAddr, 
-                        forwardOptions.srcPort, 
-                        forwardOptions.dstAddr, 
-                        forwardOptions.dstPort, (err, stream) => {
-                            connection.pipe(stream).pipe(connection);
-                    });
+        try {
+            conn = await createClient(sshOptions);
+        } catch (e) {
+            return reject(e);
+        }
+        server.on('connection', (connection) => {
 
+            if (tunnelOptions.autoClose) {
+                autoClose(server, connection);
+            }
+
+            conn.forwardOut(
+                forwardOptions.srcAddr,
+                forwardOptions.srcPort,
+                forwardOptions.dstAddr,
+                forwardOptions.dstPort, (err, stream) => {
+                    connection.pipe(stream).pipe(connection);
                 });
-                server.on('close',()=> conn.end());
-                resolve([server, conn]);
-            });
+
         });
+
+        server.on('close', () => conn.end());
+        resolve([server, conn]);
     });
 }
 
